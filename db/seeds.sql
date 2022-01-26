045176259609281e356ad6aee61416212083aef7
Drashtee <drashteeparmar@gmail.com>
2022-01-26 17:56:25 -0500

    insert department values

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+USE employeeTrackerDB;
+
+INSERT INTO department
+            (name)
+VALUES      ('Human Resources'),
+            ('Purchasing'),
+            ('Operations'),
+            ('Sales'),
+            ('Management'),
+            ('Finance'),
+            ('Accounting'),
+            ('IT'),
+            ('Research and Development'),
+            ('Customer Service');
+
+
+
+
