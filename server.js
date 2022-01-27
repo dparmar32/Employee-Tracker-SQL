cc669a4386a81cb8e8556de92e6299eb46d57b72
Drashtee <drashteeparmar@gmail.com>
2022-01-27 09:18:51 -0500

    employee track questions

--- server.js
+++ server.js
@@ -8,9 +8,88 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: '', //TODO: Remove it while pushing
+        database: 'employeeTrackerDB'
+    },
+    console.log(`Connected to the employeeTrackerDB database.`)
+);
+
+db.connect((err) => {
+    if (err) throw err;
+    empTrack();
 });
+
+
+
+function viewDepartment() {
+    let sqlQuery = 'SELECT * FROM department';
+    connection.query(sqlQuery,(err, res) => {
+        if (err) throw (err);
+        console.log('DEPARTMENTS:');
+        console.table(res);
+        empTrack()
+    })
+}
+
+function viewRole() {
+
+}
+
+function viewEmployee() {
+
+}
+
+const empTrack = () => {
+    inquirer
+        .prompt([
+            {
+                name: 'choices',
+                type: 'list',
+                message: 'What would you like to do? Choose the option from below',
+                choices: [
+                    'Add a new department',
+                    'Add a new role',
+                    'Add a new employee',
+                    'Update employee role',
+                    'View all department',
+                    'view all employee',
+                    'veiw all employee role',
+                    'View all employee by department',
+                    'Remove department',
+                    'Remove employee',
+                    'Remove role',
+                    'I am done'
+                ]
+            }
+        ])
+        .then((answer) => {
+                switch (answer.action) {
+                    case 'view all department':
+                        viewDepartment();
+                        break;
+                    case 'View all role':
+                        viewRole();
+                        break;
+                    case 'View all employee':
+                        viewEmployee();
+                        break;
+                }
+            }
+        )
+}
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = db;
